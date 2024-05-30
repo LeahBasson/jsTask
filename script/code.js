1b3d70cf089fde959a99b4ed95228b6e6837d820
LeahBasson <leahbasson54@gmail.com>
2024-05-30 10:09:53 +0200

    jsTask

---
+++ script/code.js
@@ -0,0 +1,23 @@
+let first_number = 15;
+let second_number = 17;
+
+//1 
+let third_number = first_number;
+first_number = second_number;
+second_number = third_number;
+
+console.log('first number:',first_number);
+console.log('second number:',second_number);
+
+//2
+// [first_number, second_number] = [second_number, first_number];
+// console.log('first number:', first_number);
+// console.log('second number:', second_number);
+
+//3
+// first_number = first_number ^ second_number;
+// second_number = first_number ^ second_number;
+// first_number = first_number ^ second_number;
+
+// console.log('first number:', first_number);
+// console.log('second number:',second_number);
